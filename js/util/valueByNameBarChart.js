171898d65237e9b57ba1279a5200e61afc993765
sophie <dukeyg37@gmail.com>
2020-05-05 22:29:19 -0700

    add animation

--- js/util/valueByNameBarChart.js
+++ js/util/valueByNameBarChart.js
@@ -17,13 +17,21 @@ function drawValueByNameBarChart(selection, dataUrl, yAxisLabel) {
         .attr("transform",
             "translate(" + MARGINS.left + "," + MARGINS.top + ")");
 
+    let tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html((d) => "Name: " + d.name + "<br/>" +
+            "Value: " + d.metric);
+
+    svg.call(tip);
+
     d3.csv(dataUrl, function (csv) {
         return {name: csv.name, metric: parseFloat(csv.metric)}
         },
         function(data) {
         let x = d3.scaleBand()
             .range([0, WIDTH])
-            .domain(data.map(function(d) { return d.name; }))
+            .domain(data.map((d) => d.name))
             .padding(0.2);
 
         let yMax = d3.max(data, d => d.metric);
@@ -34,7 +42,6 @@ function drawValueByNameBarChart(selection, dataUrl, yAxisLabel) {
             .selectAll("text")
             .attr("transform", "translate(-10,0)rotate(-45)")
             .style("text-anchor", "end");
-
 
         let y = d3.scaleLinear()
             .domain([0, yMax + yMax * AXIS_PARAMS.BUFFER_PERCENTAGE])
@@ -51,7 +58,7 @@ function drawValueByNameBarChart(selection, dataUrl, yAxisLabel) {
         svg.append("g")
             .call(d3.axisLeft(y));
 
-        svg.selectAll("mybar")
+        svg.selectAll("bar")
             .data(data)
             .enter()
             .append("rect")
@@ -60,8 +67,18 @@ function drawValueByNameBarChart(selection, dataUrl, yAxisLabel) {
             .attr("y", (d) => y(d.metric))
             .attr("width", x.bandwidth())
             .attr("height", (d) => HEIGHT - y(d.metric))
-            .attr("fill", "#69b3a2")
-
+            .on("mouseover", (d, i) => {
+                svg.selectAll(".rect")
+                    .filter((anotherD, anotherIdx) => anotherIdx !== i)
+                    .style('fill', "#9dcdc2");
+                tip.show(d);
+            })
+            .on("mouseout", (d, i) => {
+                svg.selectAll(".rect")
+                    .style('fill', "#69b3a2");
+                tip.show(d);
+                tip.hide(d);
+            });
     })
 }
 
